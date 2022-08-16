b4209823395f7d9b1cc3cb254f7020d751984d9a
Edidiong Udoh(TechnicalDee) <gisthive@gmail.com>
2022-08-16 05:33:59 +0100

    messaging techniques

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import json
 import random
 import requests
 import africastalking
+import asyncio
 
 username = 'sandbox'
 api_key = 'd4157382a1cc716cfd17bbbf115dc6213ff7a22286aa3f60e3254d6548cab6b7'
@@ -24,18 +25,90 @@ def sendMail(message, phone):
        [phone]
    )
 
+def option2(phone_number):
+    url = 'https://rgw.k8s.apis.ng/centric-platforms/uat/enaira-user/GetUserDetailsByPhone'
+    headers = {
+                'Content-Type': 'application/json',
+                'ClientId': '40b011dd72596c3baf51f886f952d51f'
+                }
+    request_data = {
+                "phone_number": phone_number,
+                "user_type": "USER",
+                "channel_code": "APISNG"
+                }
+    res = requests.post(url, headers=headers, json=request_data, verify=False)
+    data = res.json()['response_data']
+    try:
+        if(data['Data']['status'] == 'error'):
+            tosend = 'You are not yet registered with eNaira'
+        else:
+            tosend = "Dear "+data['first_name']+" "+data['last_name']+", \n The Account Number connected to enaira is: \n "+data['account_number']+"; "+data['relationship_bank']+". \n and your wallet address is "+data['wallet_info']['wallet_address']
+    except:
+        tosend = 'Something went wrong!'
+    sendMail(tosend, phone_number)
+    print(data)
+
+def register(text, phone_number):
+    try:
+        transf = text.split(',')
+        print(transf[3])
+        print(phone_number)
+        name = transf[0].split()
+        url = 'https://rgw.k8s.apis.ng/centric-platforms/uat/enaira-user/CreateConsumerV2'
+        headers = {
+        'Content-Type': 'application/json',
+        'ClientId': '40b011dd72596c3baf51f886f952d51f'
+        }
+        request_data = {
+            "channelCode": "APISNG",
+            "uid": transf[2],
+            "uidType": "BVN",
+            "reference": "NXG3547585HGTKJHGO",
+            "title": "Mr",
+            "firstName": name[0],
+            "middleName": name[1],
+            "lastName": name[2],
+            "userName": str(name[0])+str(name[2])+str(random.randint(90, 900)),
+            "phone": phone_number,
+            "emailId": str(name[0])+str(name[2])+str(random.randint(90, 900))+"@gmail.com",
+            "postalCode": "900110",
+            "city": "gwarinpa",
+            "address": "Lagos Estate, Abuja",
+            "countryOfResidence": "NG",
+            "tier": "2",
+            "accountNumber": transf[3],
+            "dateOfBirth": "31/12/1987",
+            "countryOfBirth": "NG",
+            "password": transf[2],
+            "remarks": "Passed",
+            "referralCode": "@imbah.01"
+            }
+        res = requests.post(url, headers=headers, json=request_data, verify=False)
+        data = res.json()['response_data']
+        print(data)
+        res = data['message']
+        return res
+    except Exception as e:
+        print(e)
+        res = 'An Error Occured'
+        return res
+
 @app.route('/delivery-reports', methods=['POST'])
-def delivery_reports():
+async def delivery_reports():
     phone_number = str(request.values.get("phoneNumber", '08035336810'))
     phone_number = phone_number.replace(' ', '')
     data = request.get_json(force=True)
+    text = request.get_json(force=True)
     
     print(f'Delivery report response...\n ${data}')
+
+    await sendMail(register(text, phone_number), phone_number)
+    await sendMail(data, phone_number)
 
     return Flask.Response(status=200) 
 
 @app.route('/', methods=['POST', 'GET'])
-def ussd_callback():
+async def ussd_callback():
     global response
     session_id = request.values.get("sessionId", None)
     service_code = request.values.get("serviceCode", None)
@@ -52,49 +125,14 @@ def ussd_callback():
         response += "4. Transfer money"
 
     elif text == '1':
-        message = "Send your REGISTER and your Full Name, BVN, Password(12 chars or more) and Account Number seperated by comma eg 'REGISTER Mr John Bassey Okon,2847592048,Pa$$word1234,2262933119'; No spaces after comma"
+        message = "Send REGISTER with your \n Full Name, BVN, Password(12 chars or more) \n and Account Number seperated by comma \n eg 'REGISTER Mr John Bassey Okon,2847592048,\nPa$$word1234,2262933119'; \n\nNo spaces after comma"
         sendMail(message, phone_number)
         response = "END Dear user, you will receive instructions on how to register for enaira shortly"
-        # response += "1. Account number \n"
-        # response += "2. Account balance"
-
-    elif text == '1*1':
-        accountNumber  = "ACC1001"
-        response = "END Your account number is " + accountNumber
-
-    elif text == '1*2':
-        balance  = "KES 10,000"
-        response = "END Your balance is " + balance
 
     elif text == '2':
+        await option2(phone_number)
         response = 'END Dear user, you will receive an SMS with your enaira details shortly'
-        try:
-            url = 'https://rgw.k8s.apis.ng/centric-platforms/uat/enaira-user/GetUserDetailsByPhone'
-            headers = {
-                'Content-Type': 'application/json',
-                'ClientId': '40b011dd72596c3baf51f886f952d51f'
-                }
-            request_data = {
-                "phone_number": phone_number,
-                "user_type": "USER",
-                "channel_code": "APISNG"
-                }
-            res = requests.post(url, headers=headers, json=request_data, verify=False)
-            data = res.json()['response_data']
-            try:
-                if(data['Data']['status'] == 'error'):
-                    tosend = 'You are not yet registered with eNaira'
-                else:
-                    tosend = "Dear "+data['first_name']+" "+data['last_name']+", The Account Number connected to enaira is: "+data['account_number']+"; "+data['relationship_bank']+". and your wallet address is "+data['wallet_info']['wallet_address']
-            except:
-                tosend = 'Something went wrong!'
-            sendMail(tosend, phone_number)
-            print(data)
-            
-            
-        except Exception as e:
-            print(e)
-            response = 'You are not yet registered with eNaira or an error occured'
+        
     elif text == '3':
         response  = "CON Select the bank or financial provider to credit from \n"
         response += "1. First Bank \n"
@@ -103,6 +141,7 @@ def ussd_callback():
         response += "4. Access Bank"
         response += "4. Titan Trust Bank"
         response += "4. Load More Banks"
+
     elif text == '4':
         response  = "CON Select the bank or financial provider to transfer monry to \n"
         response += "1. First Bank \n"
@@ -111,50 +150,12 @@ def ussd_callback():
         response += "4. Access Bank"
         response += "4. Titan Trust Bank"
         response += "4. Load More Banks"
-    else:
-        try:
-            transf = text.split(',')
-            print(transf[3])
-            print(phone_number)
-            name = transf[0].split()
-            url = 'https://rgw.k8s.apis.ng/centric-platforms/uat/enaira-user/CreateConsumerV2'
-            headers = {
-            'Content-Type': 'application/json',
-            'ClientId': '40b011dd72596c3baf51f886f952d51f'
-            }
-            request_data = {
-                "channelCode": "APISNG",
-                "uid": transf[2],
-                "uidType": "BVN",
-                "reference": "NXG3547585HGTKJHGO",
-                "title": "Mr",
-                "firstName": name[0],
-                "middleName": name[1],
-                "lastName": name[2],
-                "userName": str(name[0])+str(name[2])+str(random.randint(90, 900)),
-                "phone": phone_number,
-                "emailId": str(name[0])+str(name[2])+str(random.randint(90, 900))+"@gmail.com",
-                "postalCode": "900110",
-                "city": "gwarinpa",
-                "address": "Lagos Estate, Abuja",
-                "countryOfResidence": "NG",
-                "tier": "2",
-                "accountNumber": transf[3],
-                "dateOfBirth": "31/12/1987",
-                "countryOfBirth": "NG",
-                "password": transf[2],
-                "remarks": "Passed",
-                "referralCode": "@imbah.01"
-                }
-            res = requests.post(url, headers=headers, json=request_data, verify=False)
-            data = res.json()['response_data']
-            print(data)
-            response = data['message']
-        except Exception as e:
-            print(e)
-            response = 'An Error Occured'
 
+    elif '3*' in text:
+        response = "END Send RECEIVE with account number to 88081"
 
+    elif '4*' in text:
+        response = "END Send TRANSFER with account number to 88081"
     return response
 
 # if __name__ == '__main__':
